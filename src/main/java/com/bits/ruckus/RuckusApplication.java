e34cb3cfb10bc611a7cdc3cafd2d81d13dfecead
djamel <bouanani.ghazi@gmail.com>
2022-09-20 23:47:18 +0200

    first commit!

---
+++ src/main/java/com/bits/ruckus/RuckusApplication.java
@@ -0,0 +1,14 @@
+package com.bits.ruckus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuckusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuckusApplication.class, args);
+    }
+
+}
+
